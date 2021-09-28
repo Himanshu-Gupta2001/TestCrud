f65bb1faaeb63e751da79d32e0c431cd177c4f61
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2021-09-28 19:37:20 +0530

    first commit

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDOQ7AmJhSmeP12COo_Eea4cFJE1ihD6V4",
+  authDomain: "samplecrudproject.firebaseapp.com",
+  databaseURL: "https://samplecrudproject-default-rtdb.firebaseio.com",
+  projectId: "samplecrudproject",
+  storageBucket: "samplecrudproject.appspot.com",
+  messagingSenderId: "874713673979",
+  appId: "1:874713673979:web:f201f9adf05d4ffefc005e"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
